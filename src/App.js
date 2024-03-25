a7a9f3414d5a19903733ea30edbe7c1271c4040e
zchen125 <zchen125@stevens.edu>
2024-03-25 02:20:53 -0400

    clean code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Register from './pages/Register';
--- src/pages/BackendDemo.js
+++ src/pages/BackendDemo.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import dbHandler from '../backend/dbHandler';
 import FitbitDataComponent from '../fitbit/fitbitDataComponent';
 import { useFitbitAuth } from '../fitbit/fitbitAuth';
 
 function BackendDemo() {
-    const [UID, setUID] = useState("");
+    const [fitbitUID, setFitbitUID] = useState("");
     const [firebaseUID, setFirebaseUID] = useState("");
     const [allData, setAllData] = useState("");
     const [UIDData, setUIDData] = useState("");
@@ -15,6 +15,7 @@ function BackendDemo() {
     const [arg1, setArg1] = useState('');
     const [arg2, setArg2] = useState('');
 
+    // instance of dbHandler for the collections "users"
     const { getAllData, getDataByDocID, addData } = dbHandler({ collectionName: "users/" });
 
     // Use the useFitbitAuth hook to get the access token
@@ -24,54 +25,62 @@ function BackendDemo() {
         // Check if accessToken is available and fetch user data only if it's available
         if (accessToken) {
             const fetchData = async () => {
-                const { getProfile, getUID } = FitbitDataComponent({ accessToken });
-
+                const { getProfile, getUID, getHeartRateTimeSeries } = FitbitDataComponent({ accessToken });
+    
                 try {
                     const auth = getAuth();
-                    onAuthStateChanged(auth, (user) => {
+                    onAuthStateChanged(auth, async (user) => {
                         if (user) {
+                            // set states with Firebase user email and UID
                             setUserEmail(user.email);
                             setFirebaseUID(user.uid);
+    
+                            // set FitBit UID state
+                            const FBUID = await getUID();
+                            setFitbitUID(FBUID);
+    
+                            const sampleData = {
+                                heartrate: 123,
+                                test: "test string",
+                            };
+
+                            // set doc ID with Fitbit UID to getProfile and sampleData
+                            await addData(FBUID, await getProfile());
+                            await addData(FBUID, sampleData);
+                            
+                            // set doc ID with Firebase UID to getProfile
+                            await addData(user.uid, await getProfile());
+                            await addData(user.uid, await getHeartRateTimeSeries('2024-02-02', '1d'));
+    
+                            getDataByDocID(user.uid).then((data) => {
+                                console.log('Data by UID:', data);
+                                setUIDData(data);
+                            });
+    
+                            getAllData().then((data) => {
+                                console.log('All data from collection:', data);
+                                setAllData(data);
+                            });
                         }
                     });
-
-                    const UID = await getUID();
-                    setUID(UID);
-
-                    const sampleData = {
-                        heartrate: 123,
-                        test: "test string",
-                    };
-
-                    await addData(UID, await getProfile());
-                    await addData(UID, sampleData);
-
-                    getDataByDocID(UID).then((data) => {
-                        console.log('Data by UID:', data);
-                        setUIDData(data);
-                    });
-
-
-                    getAllData().then((data) => {
-                        console.log('All data from collection:', data);
-                        setAllData(data);
-                    });
-
+    
                 } catch (error) {
                     console.error('Error in fetching data:', error);
                 }
             };
-
+    
             fetchData();
         }
     }, [accessToken]);
-
+    
+    
+    // submit button for write data fields
     const handleSubmit = (e) => {
         e.preventDefault();
-        addData(UID, { [arg1]: arg2 })
+        addData(firebaseUID, { [arg1]: arg2 })
             .then(() => {
                 getAllData().then((data) => setAllData(data));
-                getDataByDocID(UID).then((data) => setUIDData(data));
+                getDataByDocID(firebaseUID).then((data) => setUIDData(data));
             })
             .catch((error) => {
                 console.error('Error adding data:', error);
@@ -82,11 +91,11 @@ function BackendDemo() {
         <div>
             <h1>Backend Demo</h1>
             <p><b>Firebase Auth UID: </b> {firebaseUID}</p>
-            <p><b>FitBit UID: </b> {UID}</p>
+            <p><b>FitBit UID: </b> {fitbitUID}</p>
             <p><b>User Email: </b> {userEmail}</p>
             <p><Link to="/login">Log in</Link> <Link to="/register">Register</Link></p>
             <hr />
-            <p>Write Data:
+            <p>Write Data to doc with FireStore UID:
                 <form onSubmit={handleSubmit}>
                     <label>
                         <input type="text" placeholder="field name" value={arg1} onChange={(e) => setArg1(e.target.value)} />
@@ -100,7 +109,7 @@ function BackendDemo() {
             <hr />
             <b>All data from collection: </b><pre>{JSON.stringify(allData)}</pre>
             <hr />
-            <b>Data From UID {"(should be your data)"}: </b><pre>{JSON.stringify(UIDData)}</pre>
+            <b>Data From FireStore UID {"(should be your data)"}: </b><pre>{JSON.stringify(UIDData)}</pre>
         </div>
     );
 }
