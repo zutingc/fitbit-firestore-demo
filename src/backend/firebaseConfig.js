eebec7b0f6546765b6c6b3c35bf58fb164f913f5
Zuting Chen <102315862+zutingc@users.noreply.github.com>
2024-03-29 21:28:55 -0400

    Create firebaseConfig.js

---
+++ src/backend/firebaseConfig.js
@@ -0,0 +1,20 @@
+/*
+Get this from Firebase app settings
+*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+export default firebaseConfig;
